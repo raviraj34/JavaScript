fc84886683d91a110419e6fb9639d85c86b70c6f
raviraj34 <singhraviraj19305@gmail.com>
2025-02-20 22:53:14 +0530

    modified

--- Events.js
+++ Events.js
@@ -1,5 +1,36 @@
-let button = document.getElementById("btn")
+// let button = document.getElementById("btn")
 
-button.addEventListener("click",() =>{
-    document.querySelector(".container").innerHTML  = "<b>this code is changed </b>"
-})
+// button.addEventListener("click",() =>{
+//     document.querySelector(".container").innerHTML  = "<b>this code is changed </b>"
+// })
+
+
+console.log("this is the event file");
+
+let text  = "this is the hellow world project";
+
+
+let result = text.charAt(3);
+
+console.log(result);
+
+const  friend = document.getElementsByClassName("text").innerHTML = "this is the friend";
+
+
+function data (re) {
+
+    console.log("rarly find that thing");
+}
+
+
+
+
+const eventod = document.getElementById("inputtext");
+
+const handle = (event) =>{
+    console.log(event)
+}
+
+eventod.AddEventListener("input",handle);
+
+window.WritableStream("ravi raji sinfh chandrawat")
